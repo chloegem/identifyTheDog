e94056431290eba294bd8132b464bbcb09c96c01
chloegem <chloegemayel@Chloes-MacBook-Pro.local>
2022-12-03 19:45:40 +0200

    Gender API Done

--- src/app/services/gender.service.ts
+++ src/app/services/gender.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenderService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getGender(name:string):Observable<any>{
+return this.http.get('https://api.genderize.io?name='+name);
+  }
 }
