2d89f815eff31b6bf8f22aa4749063a1128d51fb
chloegem <chloegemayel@Chloes-MacBook-Pro.local>
2022-12-03 19:47:08 +0200

    Nationality API done

--- src/app/services/nationality.service.ts
+++ src/app/services/nationality.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NationalityService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getNationality(name:string):Observable<any>{
+    return this.http.get('https://api.nationalize.io/?name='+name);
+  }
 }
