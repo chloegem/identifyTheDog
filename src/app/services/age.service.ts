63a6799f016192a5160724203686b61375d10cb7
chloegem <chloegemayel@Chloes-MacBook-Pro.local>
2022-12-03 19:44:01 +0200

    Age API done

--- src/app/services/age.service.ts
+++ src/app/services/age.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgeService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public getAge(name:string):Observable<any>{
+    return this.http.get('https://api.agify.io/?name='+name);
+  }
 }
