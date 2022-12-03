92c30b3c7f73bfe31f3cd62ffcf6b9a56d5033be
chloegem <chloegemayel@Chloes-MacBook-Pro.local>
2022-12-03 19:48:03 +0200

    New page Identify added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'identify',
+    loadChildren: () => import('./identify/identify.module').then( m => m.IdentifyPageModule)
+  },
 ];
 
 @NgModule({
