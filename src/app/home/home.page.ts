9c59dd137567d1818169853856fd454b45711e75
chloegem <chloegemayel@Chloes-MacBook-Pro.local>
2022-12-03 19:52:19 +0200

    Functions for homepage complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DogService,Dog } from '../services/dog.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public name='';
+  message='';
 
+  constructor(private dog:DogService, private router:Router) {}
+
+  ngOnInit(){
+    this.dog.getDog().subscribe((response: Dog)=>{
+      this.message=response.message;
+    });
+  }
+
+  public generate(){
+    const name = this.name.replace(/\s/g,'');
+    this.router.navigate([`/identify/${name}`]);
+  }
 }
